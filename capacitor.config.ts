bbad7073245680dca3c1623f7204bc51ad703dd8
Eric Horodyski <horodyski@ionic.io>
2022-08-22 12:49:25 -0400

    Initial Commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'featured-products',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "io.ionic.cs.portals.ecommerce.FeaturedProducts",
+  appName: "Ionic Portals Ecommerce MFE",
+  webDir: "build",
+  bundledWebRuntime: false,
 };
 
 export default config;
